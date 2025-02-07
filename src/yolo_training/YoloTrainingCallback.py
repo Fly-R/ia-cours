76e00d441f21d87c5c9a030f12f817e9a44e23ad
Romain <romain.reydel@gmail.com>
2025-02-07 22:41:20 +0100

    feat : refacto

--- src/yolo_training/YoloTrainingCallback.py
+++ src/yolo_training/YoloTrainingCallback.py
@@ -9,6 +9,20 @@ from src.file_reader.XmlPicselliaReader import XmlPicselliaReader
 
 class YoloTrainingCallback:
 
+    __PRECISION = "precision"
+    __RECALL = "recall"
+    __MAP50 = "mAP50"
+    __MAP50_95 = "mAP50-95"
+    __BOX_LOSS = "box_loss"
+    __CLS_LOSS = "cls_loss"
+    __DFL_LOSS = "dfl_loss"
+
+    __PRECISION_METRIC = "metrics/precision(B)"
+    __RECALL_METRIC = "metrics/recall(B)"
+    __MAP50_METRIC = "metrics/mAP50(B)"
+    __MAP50_95_METRIC = "metrics/mAP50-95(B)"
+
+
     def __init__(self, pics:Picsellia, xml_config:XmlPicselliaReader):
         self.__pics = pics
         self.__xml_config = xml_config
@@ -34,43 +48,43 @@ class YoloTrainingCallback:
         experiment = self.__pics.experiment
         metrics_csv = pd.read_csv(trainer.csv)
 
-        experiment.log(name="precision", data=metrics_csv["metrics/precision(B)"].tolist(), type=LogType.LINE)
-        experiment.log(name="precision", data=metrics_csv["metrics/recall(B)"].tolist(), type=LogType.LINE)
-        experiment.log(name="precision", data=metrics_csv["metrics/mAP50(B)"].tolist(), type=LogType.LINE)
-        experiment.log(name="precision", data=metrics_csv["metrics/mAP50-95(B)"].tolist(), type=LogType.LINE)
+        experiment.log(name=self.__PRECISION, data=metrics_csv[self.__PRECISION_METRIC].tolist(), type=LogType.LINE)
+        experiment.log(name=self.__RECALL, data=metrics_csv[self.__RECALL_METRIC].tolist(), type=LogType.LINE)
+        experiment.log(name=self.__MAP50, data=metrics_csv[self.__MAP50_METRIC].tolist(), type=LogType.LINE)
+        experiment.log(name=self.__MAP50_95, data=metrics_csv[self.__MAP50_95_METRIC].tolist(), type=LogType.LINE)
 
         box_loss_data = {
             'train': metrics_csv["train/box_loss"].tolist(),
             'val': metrics_csv["val/box_loss"].tolist(),
         }
-        experiment.log(name="box_loss", data=box_loss_data, type=LogType.LINE)
+        experiment.log(name=self.__BOX_LOSS, data=box_loss_data, type=LogType.LINE)
 
         cls_loss_data = {
             'train': metrics_csv["train/cls_loss"].tolist(),
             'val': metrics_csv["val/cls_loss"].tolist(),
         }
-        experiment.log(name="cls_loss", data=cls_loss_data, type=LogType.LINE)
+        experiment.log(name=self.__CLS_LOSS, data=cls_loss_data, type=LogType.LINE)
 
         dfl_loss_data = {
             'train': metrics_csv["train/dfl_loss"].tolist(),
             'val': metrics_csv["val/dfl_loss"].tolist(),
         }
-        experiment.log(name="dfl_loss", data=dfl_loss_data, type=LogType.LINE)
+        experiment.log(name=self.__DFL_LOSS, data=dfl_loss_data, type=LogType.LINE)
 
 
     def __send_metrics_on_train_epoch_end(self, trainer:DetectionTrainer):
         experiment = self.__pics.experiment
-        experiment.log(name="precision", data=float(trainer.metrics["metrics/precision(B)"]), type=LogType.LINE)
-        experiment.log(name="recall", data=float(trainer.metrics["metrics/recall(B)"]), type=LogType.LINE)
-        experiment.log(name="mAP50", data=float(trainer.metrics["metrics/mAP50(B)"]), type=LogType.LINE)
-        experiment.log(name="mAP50-95", data=float(trainer.metrics["metrics/mAP50-95(B)"]), type=LogType.LINE)
+        experiment.log(name=self.__PRECISION, data=float(trainer.metrics[self.__PRECISION_METRIC]), type=LogType.LINE)
+        experiment.log(name=self.__RECALL, data=float(trainer.metrics[self.__RECALL_METRIC]), type=LogType.LINE)
+        experiment.log(name=self.__MAP50, data=float(trainer.metrics[self.__MAP50_METRIC]), type=LogType.LINE)
+        experiment.log(name=self.__MAP50_95, data=float(trainer.metrics[self.__MAP50_95_METRIC]), type=LogType.LINE)
 
-        experiment.log(name="box_loss", data={'train':[float(trainer.loss_items[0].item())]}, type=LogType.LINE)
-        experiment.log(name="cls_loss", data={'train':[float(trainer.loss_items[1].item())]}, type=LogType.LINE)
-        experiment.log(name="dfl_loss", data={'train':[float(trainer.loss_items[2].item())]}, type=LogType.LINE)
+        experiment.log(name=self.__BOX_LOSS, data={'train':[float(trainer.loss_items[0].item())]}, type=LogType.LINE)
+        experiment.log(name=self.__CLS_LOSS, data={'train':[float(trainer.loss_items[1].item())]}, type=LogType.LINE)
+        experiment.log(name=self.__DFL_LOSS, data={'train':[float(trainer.loss_items[2].item())]}, type=LogType.LINE)
 
     def __send_metrics_on_val_end(self, trainer:DetectionValidator):
         experiment = self.__pics.experiment
-        experiment.log(name="box_loss", data={'val': [float(trainer.loss[0].item())]}, type=LogType.LINE)
-        experiment.log(name="cls_loss", data={'val': [float(trainer.loss[1].item())]}, type=LogType.LINE)
-        experiment.log(name="dfl_loss", data={'val': [float(trainer.loss[2].item())]}, type=LogType.LINE)
+        experiment.log(name=self.__BOX_LOSS, data={'val': [float(trainer.loss[0].item())]}, type=LogType.LINE)
+        experiment.log(name=self.__CLS_LOSS, data={'val': [float(trainer.loss[1].item())]}, type=LogType.LINE)
+        experiment.log(name=self.__DFL_LOSS, data={'val': [float(trainer.loss[2].item())]}, type=LogType.LINE)
